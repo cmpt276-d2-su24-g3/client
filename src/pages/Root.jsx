a6fcd01aa77249a28e663f25c7037e3fec921bca
Cayuga007 <123354205+Cayuga007@users.noreply.github.com>
2024-07-08 13:40:22 -0700

    feat: add routing (#1)
    * feat: add routing
    
    * fix: add `react-router-dom` dependency
    
    * chore!: lowercase route names
    
    * chore!: rename `Index` to `Root`
    
    * chore!: set `Root` as index route
    
    We decided to not prompt users for login. The contents of `Root` don't reflect what we'll have there.
    
    * fix: `setUser` typo
    
    ---------
    
    Co-authored-by: Jayson Tram <jaysontram@Jaysons-MBP-2.lan>
    Co-authored-by: Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>

---
+++ src/pages/Root.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+export function Root() {
+    const [user, setUser] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/')
+            .then(response => response.json())
+            .then(data => setUser(data))
+            .catch(error => console.error('Error fetching users:', error));
+    }, []);
+
+    return (
+        <div>
+            <h1>Portal</h1>
+            <h2>Welcome {user.username}</h2>
+            <Link to="/latency">Latency</Link>
+            <br></br>
+            <Link to="/logout">Logout</Link>
+        </div>
+    )
+}
