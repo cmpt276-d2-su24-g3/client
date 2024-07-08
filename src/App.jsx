bfd2a86c6a42601402d4bc7cd7dd44e42fc176d6
Jayson Tram <jaysontram@Jaysons-MBP-2.lan>
2024-07-07 21:46:16 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { LoginForm } from "./LoginForm";
+import { RegisterForm } from "./RegisterForm";
+
+export default function App() {
+  return (
+    <div>
+      <LoginForm/>
+    </div>
+  )
+}
---
+++ src/RegisterForm.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+export function RegisterForm() {
+    const [user, setUser] = useState({username: '', password: ''});
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        fetch('https://localhost:8080/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+        })
+        .then(response => response.json())
+        .then(data => console.log('User created:', data))
+        .catch(error => console.error('Error creating user:', error));
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setUser(prevUser => ({ ...prevUser, [name]: value}));
+    };
+
+    return (
+        <div>
+            <h1>Register</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="username"
+                    value={user.username}
+                    placeholder="Username"
+                    onChange={handleChange}
+                />
+                <br></br><br></br>
+                <input
+                    type="text"
+                    name="password"
+                    value={user.password}
+                    placeholder="Password"
+                    onChange={handleChange}
+                />
+                <br></br><br></br>
+                <button type="submit">Submit</button>
+            </form>
+            <br></br>
+            <a href="/login">Login</a>
+        </div>
+    )
+}
