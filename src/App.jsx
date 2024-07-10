ec2149313319fac52ec52e506e62c7117c703cc8
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2024-07-10 15:18:39 -0700

    feat: add latency map (#12)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import { Root } from './pages/Root'
 import { Login } from './pages/Login'
 import { Register } from './pages/Register'
-import { Root } from './pages/Root'
+import { Latency } from './pages/Latency'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
           <Route index element={<Root />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/latency" element={<Latency />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/Latency/index.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react'
+import { Map } from './Map'
+
+export function Latency() {
+  const [regions, setRegions] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const res = await fetch(
+          import.meta.env.VITE_API_URL + '/data/regions.json',
+        )
+        if (!res.ok) throw new Error('Failed to fetch regions')
+        const regions = await res.json()
+        setRegions(regions)
+      } catch (error) {
+        setError(error)
+      } finally {
+        setLoading(false)
+      }
+    })()
+  }, [])
+
+  return (
+    <div className="flex flex-row gap-4 p-4 h-dvh">
+      <div className="flex flex-col grow">
+        <h1 className="text-3xl">Latency</h1>
+        <h2 className="xl">
+          Get live latencies to AWS data centers worldwide.
+        </h2>
+        <div className="h-full">
+          <Map regions={regions} />
+        </div>
+      </div>
+      <div className="flex flex-col gap-2 basis-2/5">
+        <div className="container flex items-center justify-center font-bold uppercase shadow-md grow text-slate-400">
+          Location select
+        </div>
+        <div className="container flex items-center justify-center font-bold uppercase shadow-md grow text-slate-400">
+          Region select
+        </div>
+        <div className="container flex items-center justify-center font-bold uppercase shadow-md grow text-slate-400">
+          Table
+        </div>
+      </div>
+    </div>
+  )
+}
