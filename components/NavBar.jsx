64104116ebc4918756a881a9297f2cc9735daeb2
Jayson Tram <jaysontram@Jaysons-MacBook-Pro-2.local>
2024-07-16 19:10:27 -0700

    feat: add navbar

---
+++ components/NavBar.jsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom'
+
+import AWS_Logo from "@/AWS_Logo.png"
+
+export function NavBar(page) {
+    return (
+        <div className="flex justify-between py-3 bg-sky-950">
+            <div className="flex text-sm text-white">
+                <img
+                    alt="AWS"
+                    src={AWS_Logo}
+                    className="mx-5 h-6"
+                />
+                <span className="mx-5">YYC Portal</span>
+                
+                <Link className="mx-5" to="/latency">
+                    Dashboard
+                    {true && <div className="py-1 bg-orange-500"></div>}
+                </Link>
+                <Link className="mx-5" to="/latency">
+                    Service Availability
+                    {page == "Service Availability" && <div className="py-1 bg-orange-500"></div>}
+                </Link>
+                <Link className="mx-5" to="/latency">
+                    Latency History
+                    {page == "Latency History" && <div className="py-1 bg-orange-500"></div>}
+                </Link>
+            </div>
+            <span className="mx-5 text-white">Name</span>
+        </div>
+    )
+}
