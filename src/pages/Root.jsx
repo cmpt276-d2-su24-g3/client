169b178051676083b9dfedf510e4e83d81cf876a
Jayson Tram <jaysontram@Jaysons-MBP-2.lan>
2024-07-28 12:34:17 -0700

    feat: add image and description toggling

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -3,15 +3,46 @@ import { Link } from 'react-router-dom'
 import { NavBar } from '@/components/NavBar'
 
 import WorldMap from '../assets/world-map.svg'
-import R2R from '../assets/r2r-image.svg'
+import C2R_Image from '../assets/c2r-image.svg'
+import R2R_Image from '../assets/r2r-image.svg'
+import R2C_Image from '../assets/r2c-image.svg'
+import Chatbox_Image from '../assets/chatbot-image.svg'
 
 import '../assets/fonts.css'
 import { Info } from 'lucide-react'
 import { info } from 'autoprefixer'
 
+const Descriptions = {
+  "Amazon LQ": `Amazon Q brings its advanced generative AI technology to Amazon QuickSight,
+                the AWS unified business intelligence (BI) service built for the cloud.
+                With Amazon Q in QuickSight, customers get a generative BI assistant that allows
+                analysts to use natural language to build BI dashboards in minutes and easily
+                create visualizations and complex calculations.`,
+  "Client to Region": `Amazon Q brings its advanced generative AI technology to Amazon QuickSight,
+                       the AWS unified business intelligence (BI) service built for the cloud.
+                       With Amazon Q in QuickSight, customers get a generative BI assistant that allows
+                       analysts to use natural language to build BI dashboards in minutes and easily
+                       create visualizations and complex calculations.`,
+  "Region to Region": `Amazon Q brings its advanced generative AI technology to Amazon QuickSight,
+                       the AWS unified business intelligence (BI) service built for the cloud.
+                       With Amazon Q in QuickSight, customers get a generative BI assistant that allows
+                       analysts to use natural language to build BI dashboards in minutes and easily
+                       create visualizations and complex calculations.`,
+  "Region to Client": `Amazon Q brings its advanced generative AI technology to Amazon QuickSight,
+                       the AWS unified business intelligence (BI) service built for the cloud.
+                       With Amazon Q in QuickSight, customers get a generative BI assistant that allows
+                       analysts to use natural language to build BI dashboards in minutes and easily
+                       create visualizations and complex calculations.`,
+  "Chatbot": `Amazon Q brings its advanced generative AI technology to Amazon QuickSight,
+                       the AWS unified business intelligence (BI) service built for the cloud.
+                       With Amazon Q in QuickSight, customers get a generative BI assistant that allows
+                       analysts to use natural language to build BI dashboards in minutes and easily
+                       create visualizations and complex calculations.`,
+}
+
 const InfoButton = ({ text, infoHighlighted, setInfoHighlighted }) => {
   return (
-    <button onClick={() => setInfoHighlighted(text)} className={infoHighlighted == text ? 'p-4 bg-blue-500 rounded-full' : 'p-4'}>
+    <button onClick={() => setInfoHighlighted(text)} className={infoHighlighted == text ? 'p-4 bg-blue-400 rounded-full' : 'p-4'}>
       {text}
     </button>
   )
@@ -23,13 +54,7 @@ export function Root() {
   return (
     <div className="bg-sky-50 min-h-screen">
       <NavBar page="YYC Portal"/>
-      <Link to="/admin">Admin</Link>
-      <div className='flex justify-end'>
-        <button className='p-1 px-3 m-4 border-4 border-blue-500 rounded-full text-blue-500'>client-to-region</button>
-        <button className='p-1 px-3 m-4 border-4 border-blue-500 rounded-full text-blue-500'>region-to-region</button>
-        <button className='p-1 px-3 m-4 border-4 border-blue-500 rounded-full text-blue-500'>region-to-client</button>
-      </div>
-      <div className='flex justify-evenly'>
+      <div className='flex justify-evenly items-center mt-10'>
         <div className='p-5'>
           <span className='m-3 text-5xl font-bold'>YYC Portal</span>
           <p className='m-4'>
@@ -38,7 +63,7 @@ export function Root() {
             efficient and reliable network performance.
           </p>
           <button className='p-4 m-3 rounded-full text-white bg-blue-700'>
-            <Link to="/latency">Get Started</Link>
+            <a href="https://yyc-portal.auth.us-west-2.amazoncognito.com/login?client_id=481g1a0ridauh779f34tvsti05&response_type=code&scope=email+openid&redirect_uri=https%3A%2F%2Fdd06gmwf4nvya.cloudfront.net%2Flogin">Get Started</a>
           </button>
         </div>
         <div className='w-1/2'>
@@ -51,23 +76,31 @@ export function Root() {
             <InfoButton text="Client to Region" infoHighlighted={infoHighlighted} setInfoHighlighted={setInfoHighlighted}/>
             <InfoButton text="Region to Region" infoHighlighted={infoHighlighted} setInfoHighlighted={setInfoHighlighted}/>
             <InfoButton text="Region to Client" infoHighlighted={infoHighlighted} setInfoHighlighted={setInfoHighlighted}/>
+            <InfoButton text="Chatbot" infoHighlighted={infoHighlighted} setInfoHighlighted={setInfoHighlighted}/>
         </div>
       </div>
       <div className='flex justify-center mt-10'>
-        <div>
+        <div className='m-4'>
           <span className='text-2xl font-bold'>{infoHighlighted}</span>
-          <p>
-            Amazon Q brings its advanced generative AI technology to Amazon QuickSight,<br></br>
-            the AWS unified business intelligence (BI) service built for the cloud.<br></br>
-            With Amazon Q in QuickSight, customers get a generative BI assistant that allows<br></br>
-            analysts to use natural language to build BI dashboards in minutes and easily<br></br>
-            create visualizations and complex calculations.
-          </p>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: Descriptions[infoHighlighted].replace(/\n/g, '<br>')
+            }}
+          ></p>
         </div>
         <div>
-          <img src={R2R}></img>
+          <img className='h-64' src={
+            infoHighlighted == "Amazon LQ" ? C2R_Image
+              : infoHighlighted == "Client to Region" ? C2R_Image
+              : infoHighlighted == "Region to Region" ? R2R_Image
+              : infoHighlighted == "Region to Client" ? R2C_Image
+              : infoHighlighted == "Chatbot" ? Chatbox_Image
+              : Chatbox_Image
+          }
+          ></img>
         </div>
       </div>
+      <Link to="/admin">Admin</Link>
     </div>
   )
 }
