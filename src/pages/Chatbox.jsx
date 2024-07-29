e4eadf8d0dcd8c716fb447debed792a45412f27c
Jayson Tram <jaysontram@Jaysons-MBP-2.lan>
2024-07-29 16:49:41 -0700

    feat: add design

--- src/pages/Chatbox.jsx
+++ src/pages/Chatbox.jsx
@@ -1,4 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import { NavBar } from '@/components/NavBar'
+
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
 
 export function Chatbox() {
   const [input, setInput] = useState('');
@@ -21,7 +30,7 @@ export function Chatbox() {
       time: new Date().toISOString()  // ISO 8601
     };
 
-    const response = await fetch('http://zefta.catalpa.pw:8000/chat', {
+    const response = await fetch('http://zefta.catalpa.pw:8000/fake-chat', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -40,23 +49,55 @@ export function Chatbox() {
     }
   };
 
+  const handleGetHistory = async () => {};
+
+  const handleDeleteHistory = async () => {};
+
   return (
     <div>
-      <h1>Streamed Data</h1>
-      <input
-        type="text"
-        value={input}
-        onChange={handleInputChange}
-        placeholder="Enter your input"
-      />
-      <input
-        type="text"
-        value={uuid}
-        onChange={handleUuidChange}
-        placeholder="Enter your UUID v4"
-      />
-      <button onClick={handleButtonClick}>Send</button>
-      <pre>{data}</pre>
+      <NavBar page="Chatbox" />
+      <div className="flex justify-center items-center h-screen bg-sky-950">
+        <div className='flex flex-col justify-between items-center w-2/3 h-4/5'>
+          <span className="text-6xl font-bold text-sky-500">
+              Chatbot
+          </span>
+          <p className='p-5 m-10 w-full h-full bg-sky-900 text-white'>
+            {data}
+          </p>
+          <div className='flex w-full'>
+            <input
+              type="text"
+              value={uuid}
+              onChange={handleUuidChange}
+              placeholder="Enter your UUID v4"
+              className='p-2 m-2 rounded-full bg-sky-900 text-center text-white'
+            />
+            <div className='flex flex-1 p-2 m-2 rounded-full bg-sky-900'>
+              <input
+                type="text"
+                value={input}
+                onChange={handleInputChange}
+                placeholder="Enter your input"
+                className='pl-4 w-3/4 bg-transparent text-white'
+              />
+              <button className='flex-1 text-white' onClick={handleButtonClick}>Send</button>
+            </div>
+            <Menubar className='mt-2'>
+              <MenubarMenu>
+                <MenubarTrigger>...</MenubarTrigger>
+                <MenubarContent>
+                  <MenubarItem>
+                    <button onClick={handleGetHistory}>Get History</button>
+                  </MenubarItem>
+                  <MenubarItem>
+                    <button onClick={handleDeleteHistory}>Delete History</button>
+                  </MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+            </Menubar>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
