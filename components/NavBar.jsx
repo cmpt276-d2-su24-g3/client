692510e75c7ee256418e7ab70c2588eab602b6c0
reinesana <17shana2017@gmail.com>
2024-09-30 18:01:56 -0700

    documentation in navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,11 +1,9 @@
-import { Link } from 'react-router-dom'
-
-import AWS_Logo from "@/AWS_Logo.png"
-
+import { Link } from 'react-router-dom';
+import AWS_Logo from "@/AWS_Logo.png";
 
 export function NavBar({ page }) {
     return (
-        <div className=" z-20 fixed overflow-hidden top-0 left-0 w-full justify-between py-4  bg-customNav shadow-[0_4px_6px_-1px_rgba(255,255,255,0.2),0_2px_4px_-2px_rgba(255,255,255,0.8)]">
+        <div className="z-20 fixed overflow-hidden top-0 left-0 w-full flex justify-between py-4 bg-customNav shadow-[0_4px_6px_-1px_rgba(255,255,255,0.2),0_2px_4px_-2px_rgba(255,255,255,0.8)]">
             <div className="flex text-sm text-white items-center">
                 <img
                     alt="AWS"
@@ -29,13 +27,19 @@ export function NavBar({ page }) {
                     {page === "Chatbox" && <div className="py-1 bg-orange-500"></div>}
                 </Link>
             </div>
+
+            {/* Button added at the end of the navbar */}
+            <div className="mr-8 p-0 bg-white rounded-full font-ember">
+                <button className="bg-white text-sm py-1.5 font-ember px-8 py-0 rounded-full transition-all duration-300 ease-in-out hover:shadow-[0_0_15px_5px_rgba(75,0,130,0.7)]">
+                    documentation
+                </button>
+            </div>
+
+
+
             
+
+
         </div>
-
-
-
-
-    )
+    );
 }
-
-
